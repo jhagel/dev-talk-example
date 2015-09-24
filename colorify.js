fc6bbba59d783ca096801f76cbd2276882d09642
Josh Hagel <josh@14four.com>
2015-09-24 09:22:47 -0700

    Add shade ability

--- colorify.js
+++ colorify.js
@@ -2,7 +2,10 @@
 
     var shade = "#556b2f";
 
-    $.fn.colorify = function() {
+    $.fn.colorify = function(newShade) {
+        if ( typeof newShade != 'undefined') {
+            shade = newShade;
+        }
         this.css( "color", shade );
         return this;
     };
